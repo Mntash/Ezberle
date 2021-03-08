d83986920613149937cd0423005d01ec27303aa8
Cleveyton <mtash3469@gmail.com>
2021-03-08 21:27:44 +0300

    edit

--- my_dict/asda.py
+++ my_dict/asda.py
@@ -1,2 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+github = "https://github.com/first20hours/google-10000-english/blob/master/google-10000-english-usa-no-swears-long.txt"
+html_tur = requests.get(github).content
+soup_tur = BeautifulSoup(html_tur, 'lxml')
+tds = soup_tur.find_all('td', class_='blob-code')[530:540]
+print(tds)
 
 
