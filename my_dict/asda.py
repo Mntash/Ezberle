175cabdede5645997d9492f1dbcf3f622b573ade
Cleveyton <mtash3469@gmail.com>
2021-03-08 22:35:41 +0300

    edit

--- my_dict/asda.py
+++ my_dict/asda.py
@@ -2,10 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 
-github = "https://github.com/first20hours/google-10000-english/blob/master/google-10000-english-usa-no-swears-long.txt"
+github = "https://github.com/first20hours/google-10000-english/blob/master/google-10000-english-usa-no-swears-medium.txt"
 html_tur = requests.get(github).content
 soup_tur = BeautifulSoup(html_tur, 'lxml')
-tds = soup_tur.find_all('td', class_='blob-code')[1609]
+tds = soup_tur.find_all('td', class_='blob-code')[234]
 print(tds)
 
 
