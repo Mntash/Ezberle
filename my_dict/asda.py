5e40f4d91e349c1051bc661854094aec1b878ddb
Cleveyton <mtash3469@gmail.com>
2021-03-08 23:13:03 +0300

    edit

--- my_dict/asda.py
+++ my_dict/asda.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 github = "https://github.com/first20hours/google-10000-english/blob/master/google-10000-english-usa-no-swears-medium.txt"
 html_tur = requests.get(github).content
 soup_tur = BeautifulSoup(html_tur, 'lxml')
-tds = soup_tur.find_all('td', class_='blob-code')[234]
+tds = soup_tur.find_all('td', class_='blob-code')[1680]
 print(tds)
 
 
--- my_dict/tasks.py
+++ my_dict/tasks.py
@@ -128,7 +128,7 @@ def add_words2_db():
     github = "https://github.com/first20hours/google-10000-english/blob/master/google-10000-english-usa-no-swears-medium.txt"
     html_tur = requests.get(github).content
     soup_tur = BeautifulSoup(html_tur, 'lxml')
-    tds = soup_tur.find_all('td', class_='blob-code')[1680:2000]
+    tds = soup_tur.find_all('td', class_='blob-code')[2000:2500]
     for td in tds:
         try:
             response = requests.request("GET", url, headers=headers, params={'entry': td.text})
