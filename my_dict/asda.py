01d01fcfc9b835584ae9da4aeafdc7965113cb5a
Cleveyton <mtash3469@gmail.com>
2021-03-08 21:40:15 +0300

    edit

--- my_dict/asda.py
+++ my_dict/asda.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 github = "https://github.com/first20hours/google-10000-english/blob/master/google-10000-english-usa-no-swears-long.txt"
 html_tur = requests.get(github).content
 soup_tur = BeautifulSoup(html_tur, 'lxml')
-tds = soup_tur.find_all('td', class_='blob-code')[530:540]
+tds = soup_tur.find_all('td', class_='blob-code')[953]
 print(tds)
 
 
