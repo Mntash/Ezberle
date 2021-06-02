ae8bb869cc2afb0de632141c5f4efc479591e26b
Cleveyton <mtash3469@gmail.com>
2021-06-02 17:54:38 +0300

    edit

--- my_dict/models.py
+++ my_dict/models.py
@@ -160,7 +160,7 @@ class ShopProducts(models.Model):
         ("bg-img", 'Arka plan resim'),
         ("rights", 'Haklar')
     ]
-    uuid = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     text = models.TextField()
     price = models.IntegerField(default=0)
     type = models.CharField(max_length=15, choices=product_types, default='Renk')
@@ -172,7 +172,7 @@ class ShopProducts(models.Model):
 
 
 class ProductTracker(models.Model):
-    uuid = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     text = models.TextField(default="")
     type = models.CharField(max_length=30, default="")
