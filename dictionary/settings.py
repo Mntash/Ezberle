6faadfaf1e17a74ffd18995ecd0b720d1e1c6bd6
Cleveyton <mtash3469@gmail.com>
2021-06-20 17:59:31 +0300

    edit

--- dictionary/settings.py
+++ dictionary/settings.py
@@ -28,6 +28,41 @@ SECRET_KEY = '+$*jtuqrjxh=-q2_!(%q96k_etxmx12!udcj578@+9*me@bb8='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
 ALLOWED_HOSTS = ['ezberle.herokuapp.com', 'localhost', '127.0.0.1']
 
 
