a6dd94f9cc1fc1de4a0dc21611c78eaf15d5f9bf
Cleveyton <mtash3469@gmail.com>
2021-07-15 15:00:27 +0300

    edit

--- my_dict/tests.py
+++ my_dict/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from bs4 import BeautifulSoup
 import lxml
 import cchardet
+import requests
 
 
 class SearchTurengTests(TestCase):
@@ -9,9 +10,14 @@ class SearchTurengTests(TestCase):
     saurus_url = 'https://www.thesaurus.com/browse/{}'
     data_tur = []
 
-    def test_tureng_search(self, word):
-        response = self.client.get(self.tureng_url.format(word))
-        self.assertEquals(response.status_code, 200)
+    def test_tureng_search(self):
+        response = self.client.get(self.tureng_url.format("word"))
+        # self.assertEquals(response.status_code, 200)
+        html_tur = requests.get(self.tureng_url.format("asdghj")).content
+        soup_tur = BeautifulSoup(html_tur, 'lxml')
+        self.assertTrue(soup_tur.find('audio', {'id': 'turengVoiceENTRENus'}))
 
-    test_tureng_search("word")
+
+
+
 
